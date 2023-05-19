d9804ce32bb15c02a2ef53e5ab7d122b4345aa9f
Yagmur Solmaz <52479605+yaagmurss@users.noreply.github.com>
2023-05-19 14:33:06 +0300

    variables added

---
+++ Variables.py
@@ -0,0 +1,13 @@
+#VARIABLES#
+
+#String
+name="Yagmur"
+surName="Solmaz"
+fullName=name+" "+surName
+print("Hello " + fullName)
+
+#Integer#
+age=21
+print("My age is "+ str(age))
+print("My age was "+ str(age-1))
+print("My age will be "+ str(age+1))
