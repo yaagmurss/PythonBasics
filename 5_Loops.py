7be750c6286db5de68ce7e84ff60a5dacc468f53
Yagmur Solmaz <52479605+yaagmurss@users.noreply.github.com>
2023-05-20 13:30:27 +0300

    loops control statements added

--- 5_Loops.py
+++ 5_Loops.py
@@ -1,13 +1,79 @@
 #While Loop#
-#EXAMPLE 1#
-#Loops through until entering a name
+#A statement that will execute it is block of code in a unlimited amount of time
 
 name=""
-
 while len(name)==0:
     name = input("Enter your name:")
-
 print("Hello " + name)
 
 
-#Left at 01:10:26
+#For Loop#
+#A statement that will execute it is block of code in a limited amount of time
+
+print("---------------------")
+for i in range(10):
+    print(i)
+print("---------------------")
+for i in range(50, 100+1, 2):
+    print(i)
+print("---------------------")
+for i in "Hello world":
+    print(i)
+print("---------------------")
+
+for i in range(500, 100, -20):
+    print(i)
+print("---------------------")
+
+#Nested loops
+#The inner loop will finish all of it is iterations before finishing one iteration of the outer loop.
+
+rows = int(input("How many rows? "))
+columns = int(input("How many columns? "))
+symbol = input("Enter a symbol to use? ")
+
+for i in range(rows):
+    for j in range(columns):
+        print(symbol, end="")
+    print()
+
+# Loop Control Statements = change a loops execution from it is normal sequence
+
+# break = used to terminate the loop entirely
+# continue = skips to the next iteration of the loop
+# pass = does nothing act as a placeholder
+
+#Break
+while True:
+    name = input("Please enter your name:")
+    if name!="":
+        break
+
+#Continue
+phoneNumber = "123-456-789"
+for i in phoneNumber:
+    if i == "-":
+        continue
+    print(i, end="")
+
+#Pass
+for i in range(15):
+    if i == 5:
+        pass
+    else:
+        print(i, end="-")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Notes.md
@@ -0,0 +1,10 @@
+# Notes
+
+
+•    Ctrl   Slash  : for single line comments (  //...  )
+•    Ctrl   Shift   Slash  : for block comments (  /*...*/  )
+
+
+• Press   Ctrl   D   in the editor to duplicate the selected code block, or the current line when no block is selected.
+
+• Press   Ctrl   Shift   V   to select the text fragment that you have previously copied to the clipboard.
