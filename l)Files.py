6a7170d18f39f9dc4d96ec5c169174ccfcb1851a
Yagmur Solmaz <52479605+yaagmurss@users.noreply.github.com>
2023-05-26 14:04:39 +0300

    file operations updated

--- l)Files.py
+++ l)Files.py
@@ -1,18 +1,45 @@
 import os
+import shutil
 
-#File.Exists
+# File.Exists
 
-path = "C:/Users/500500/Desktop/Docs"
+folderPath = "C:/Users/500500/Desktop/Docs"
+filePath = "C:/Users/200741/Desktop/Docs/test.txt"
+copyFilePath = "C:/Users/200741/Desktop/Docs/copy.txt"
 
-if os.path.exists(path):
+if os.path.exists(folderPath):
     print("Path exists")
 else:
     print("Path is not exists.")
 
-if os.path.isfile(path):
+if os.path.isfile(folderPath):
     print("This is a file.")
-elif os.path.isdir(path):
+elif os.path.isdir(folderPath):
     print("This is a folder.")
 
-#https://www.youtube.com/watch?v=XKHEtdqhLK8&list=RDCMUC4SVo0Ue36XCfOyb5Lh1viQ&start_radio=1
-#02:04:33
+# Read a file
+
+try:
+    with open(filePath) as file:
+        result = file.read()
+        print(result)
+except FileNotFoundError:
+    print("File not found")
+
+
+# Write a file
+
+text = "This is a some new text."
+
+with open(filePath, "w") as file:
+    file.write(text)
+
+
+# Copy File
+
+# copyfile() = copies contents of a file
+# copy() = copyfile - permission mode - destination can be directory
+# copy2() = copy() + copies metadata (file's creation and modifications times )
+
+
+shutil.copyfile(filePath, copyFilePath)
