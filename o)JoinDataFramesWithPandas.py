9cb808f4885a13185f480ef56b99cec82750e50f
Yagmur Solmaz <52479605+yaagmurss@users.noreply.github.com>
2023-12-06 10:01:52 +0300

    Create o)JoinDataFramesWithPandas.py

---
+++ o)JoinDataFramesWithPandas.py
@@ -0,0 +1,14 @@
+import pandas as pd
+# First DataFrame
+df1 = pd.DataFrame({'id': ['A01', 'A02', 'A03', 'A04'],
+					'Name': ['ABC', 'PQR', 'DEF', 'GHI']})
+
+# Second DataFrame
+df2 = pd.DataFrame({'id': ['B05', 'B06', 'B07', 'B08'],
+					'Name': ['XYZ', 'TUV', 'MNO', 'JKL']})
+
+
+frames = [df1, df2]
+
+result = pd.concat(frames)
+display(result)
